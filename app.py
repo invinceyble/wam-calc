2b1d6737cc06abe971d7036d8981fd19b7f8fea4
Vincey Au <invinceyble@users.noreply.github.com>
2018-06-24 16:43:49 +1000

    basic csv upload working

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from calc import CSVParser
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -6,6 +9,17 @@ app = Flask(__name__)
 def index():
     data = {'WAM': 85.345}
     return render_template("index.html", data=data)
+
+@app.route("/", methods=['POST'])
+def upload():
+    file = request.files.get('uploaded_file')
+    data = calc_wam(file)
+    return render_template("index.html", data=data)
+
+def calc_wam(file):
+    parser = CSVParser()
+    parser.read_transcript(file)
+    return {'WAM': parser.calc_wam()}
 
 if __name__ == "__main__":
     app.run(debug=True)
